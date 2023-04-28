2f45ad209ba1ae63ef352a75e3d7b2a01c8be127
Accurast <siavash65n@gmail.com>
2023-04-27 18:33:48 -0700

    code updates

--- Js/script.js
+++ Js/script.js
@@ -3,7 +3,6 @@ var queEl = document.getElementById("question")
 var pEl = document.getElementById("note")
 var strtEl = document.getElementById("start")
 var scoreEl = document.getElementById("score")
-//var butEl = document.querySelectorAll(".button")
 var answEl= document.querySelector("ol")
 var fdbkEl = document.querySelector(".outcome")
 
@@ -42,10 +41,7 @@ strtEl.setAttribute("style", "display:flex")
 
 
    
-strtEl.addEventListener("click", function() {
-
-//event.preventDefault();
-
+strtEl.addEventListener("click", function() {  
 
 var secondsleft = 100;
 
@@ -67,14 +63,11 @@ var timeInterval = setInterval(function(){
 
 }
 
-setTime();
-
-
 var questionList = [
     {
        List: "Commonly used data types Do Not include: ",
        answer: ["1. strings","2. boleans","3. alerts","4. numbers"],
-       correct: "2. alerts",
+       correct: "3. alerts",
    },
 
 
@@ -86,22 +79,22 @@ var questionList = [
 
 
    {
-       List: "The condition in an if/ else statement is enclosed within ______.",
-       answer: ["1. quotes","2. curlybracket","3. parantheses","4. square brackets"],
-       correct: "3. parantheses",
+       List: "Array in Javascript can be used to store _____.",
+       answer: ["1. numbers and strings","2. other arrays ","3. boleans","4. all of the above"],
+       correct: "4. all of the above",
    },
 
 
    {
-       List: "The condition in an if/ else statement is enclosed within ______.",
-       answer: ["1. quotes","2. curlybracket","3. parantheses","4. square brackets"],
-       correct: "3. parantheses",
+       List: "String values must be enclosed within _____ when being assigned to variables.",
+       answer: ["1. commas","2. curlybracket","3. quotes","4. parantheses"],
+       correct: "3. quotes",
    },
 
    {
-       List: "The condition in an if/ else statement is enclosed within ______.",
-       answer: ["1. quotes","2. curlybracket","3. parantheses","4. square brackets"],
-       correct: "3. parantheses",
+       List: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+       answer: ["1. Javascript","2. terminal/bash","3. for loops","4. console.log"],
+       correct: "4. console.log",
    },
 ];
 
@@ -110,34 +103,45 @@ strtEl.setAttribute("style", "display: none")
 answEl.setAttribute("style", "display: relative");
 queEl.setAttribute("style", "font-size: 150%");
 
-var questionIndex = 0; 
 
-    queEl.textContent = questionList[questionIndex].List
-    answEl.children[0].textContent = questionList[questionIndex].answer[0];
-    answEl.children[1].textContent = questionList[questionIndex].answer[1];
-    answEl.children[2].textContent = questionList[questionIndex].answer[2];
-    answEl.children[3].textContent = questionList[questionIndex].answer[3];
 
-//event.stopPropagation();
-answEl.children[0].addEventListener("click", function(event){
+var i = 0; 
+
+    queEl.textContent = questionList[i].List
+    answEl.children[0].textContent = questionList[i].answer[0];
+    answEl.children[1].textContent = questionList[i].answer[1];
+    answEl.children[2].textContent = questionList[i].answer[2];
+    answEl.children[3].textContent = questionList[i].answer[3];
+
+    answEl.children[0].addEventListener("click", function(event){
     console.log(event);
-    if (event.target.outerText === questionList[questionIndex].correct){    
-    questionIndex+=questionIndex++
-     }}),
+    console.log(event.target.outerText);
+    console.log(questionList[i].correct);
+    if (event.target.outerText === questionList[i].correct){    
+    i=i++
+     }})
 answEl.children[1].addEventListener("click", function(event){
     console.log(event);
-    if (event.target.outerText === questionList[questionIndex].correct){    
-    questionIndex+=questionIndex++
-    }}),
+    console.log(event.target.outerText);
+    if (event.target.outerText === questionList[i].correct){    
+    i=i++
+    }})
 answEl.children[2].addEventListener("click", function(event){
     console.log(event);
-    if (event.target.outerText === questionList[questionIndex].correct){    
-    questionIndex+=questionIndex++
-    }}),
+    console.log(event.target.outerText);
+    if (event.target.outerText === questionList[i].correct){    
+    i=i++
+    }})
 answEl.children[3].addEventListener("click", function(event){
     console.log(event);
-    if (event.target.outerText === questionList[questionIndex].correct){    
-    questionIndex+=questionIndex++
-    }}),
+    console.log(event.target.outerText);
+    if (event.target.outerText === questionList[i].correct){    
+    i=i++
+    }})
+
+
+    setTime();
 }
-);
+
+
+)
