9e601cfce63d8100298b335d4d6fd38b3b0027bd
Accurast <siavash65n@gmail.com>
2023-04-29 01:11:08 -0700

    updated

--- Js/script.js
+++ Js/script.js
@@ -18,139 +18,107 @@ strtEl.setAttribute("style", "display:flex");
 resEl.setAttribute("style", "display: none");
 
 
-   
-strtEl.addEventListener("click", function() {  
-
 var i = 0; 
+var secondsleft = 100; 
+   
+strtEl.addEventListener("click", function startQuiz() {  
 
-var questionList = [
-    {
-       List: "Commonly used data types Do Not include: ",
-       answer: ["1. strings","2. boleans","3. alerts","4. numbers"],
-       correct: "3. alerts",
-   },
-
-
-   {
-       List: "The condition in an if/ else statement is enclosed within ______.",
-       answer: ["1. quotes","2. curlybracket","3. parantheses","4. square brackets"],
-       correct: "3. parantheses",
-   },
-
-
-   {
-       List: "Array in Javascript can be used to store _____.",
-       answer: ["1. numbers and strings","2. other arrays ","3. boleans","4. all of the above"],
-       correct: "4. all of the above",
-   },
-
-
-   {
-       List: "String values must be enclosed within _____ when being assigned to variables.",
-       answer: ["1. commas","2. curlybracket","3. quotes","4. parantheses"],
-       correct: "3. quotes",
-   },
-
-   {
-       List: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-       answer: ["1. Javascript","2. terminal/bash","3. for loops","4. console.log"],
-       correct: "4. console.log",
-   },
-];
-
-pEl.setAttribute("style", "display: none")
-strtEl.setAttribute("style", "display: none")
-answEl.setAttribute("style", "display: relative");
-queEl.setAttribute("style", "font-size: 150%");
+    pEl.setAttribute("style", "display: none");
+    strtEl.setAttribute("style", "display: none");
+    answEl.setAttribute("style", "display: relative");
+    queEl.setAttribute("style", "font-size: 150%");
 
 
 
-    var secondsleft = 100;
 
-    function setTime() {
+console.log(i); 
+
+
+
+
     
     var timeInterval = setInterval(function(){
-        secondsleft--;
+         
+        if (secondsleft > 0){secondsleft--};
+        
+        if(secondsleft === 0) {
+    
+        clearInterval(timeInterval);   
+     
+        scoreMessage();
+        };
+
     
         timeEl.textContent = "Time :" + secondsleft
     
-        if(secondsleft === 0) {
-    
-            clearInterval(timeInterval);
-    
-           // scoreMessage();
-        }
-    
-    },1000);
-    
-    }    
+       
+},1000);
 
 
-    function quiz() {
 
-    queEl.textContent = questionList[i].List;
-    answEl.children[0].textContent = questionList[i].answer[0];
-    answEl.children[1].textContent = questionList[i].answer[1];
-    answEl.children[2].textContent = questionList[i].answer[2];
-    answEl.children[3].textContent = questionList[i].answer[3];
+           var questionList = [
+            {
+               List: "Commonly used data types Do Not include: ",
+               answer: ["1. strings","2. boleans","3. alerts","4. numbers"],
+               correct: "3. alerts",
+            },
+        
+        
+           {
+               List: "The condition in an if/ else statement is enclosed within ______.",
+               answer: ["1. quotes","2. curlybracket","3. parantheses","4. square brackets"],
+               correct: "3. parantheses",
+           },
 
-    answEl.children[0].addEventListener("click", function(event){
-    console.log(event);
-    console.log(event.target.outerText === questionList[i].correct);
-    if (event.target.outerText === questionList[i].correct){
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "correct!";
-        return i++;  
-     } else {
-        secondsleft = secondsleft-10;
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "wrong!";
-        return i++;
-    }})
-    answEl.children[1].addEventListener("click", function(event){
-    console.log(event);
-    console.log(event.target.outerText === questionList[i].correct);
-    if (event.target.outerText === questionList[i].correct){
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "correct!";
-        return i++; 
+           {
+                List: "Arrays in JavaScript can be used to store ____.",
+                answer: ["1. numbers and strings","2. other arrays","3.booleans","4. all of the above"],
+                correct: "4. all of the above",
+            },
+
+           {
+               List: "String values must be enclosed within _____ when being assigned to variables.",
+               answer: ["1. commas","2. curlybracket","3. quotes","4. parantheses"],
+               correct: "3. quotes",
+           },
+        
+           {
+               List: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+               answer: ["1. Javascript","2. terminal/bash","3. for loops","4. console.log"],
+               correct: "4. console.log",
+           },
+        ];
+  
+        console.log(i);      
+
+
+   queEl.textContent = questionList[i].List;
+   answEl.children[0].textContent = questionList[i].answer[0];
+   answEl.children[1].textContent = questionList[i].answer[1];
+   answEl.children[2].textContent = questionList[i].answer[2];
+   answEl.children[3].textContent = questionList[i].answer[3];
+
+   answEl.addEventListener("click", function(event){
+    console.log(event.target.outerText)    
+   if (event.target.outerText === questionList[i].correct){
+       fdbkEl.setAttribute("style", "visibility:visible");
+       fdbkEl.textContent = "correct!";
+       clearInterval(timeInterval)
+       while (i < questionList.length-1) {return startQuiz(i++)} 
+       ;
     } else {
-        secondsleft = secondsleft-10;
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "wrong!";
-        return i++; 
-    }})
-    answEl.children[2].addEventListener("click", function(event){
-    console.log(event);
-    console.log(event.target.outerText === questionList[i].correct);
-    if (event.target.outerText === questionList[i].correct){
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "correct!";
-        return i++;
-    } else {
-        secondsleft = secondsleft-10;
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "wrong!";
-        return i++; 
-    }})
-    answEl.children[3].addEventListener("click", function(event){
-    console.log(event);
-    console.log(event.target.outerText === questionList[i].correct);
-    if (event.target.outerText === questionList[i].correct){    
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "correct!";
-        return i++; 
-    } else {
-        secondsleft = secondsleft-10;
-        fdbkEl.setAttribute("style", "visibility:visible");
-        fdbkEl.textContent = "wrong!";
-        return i++; 
-    }})
-    
-    
-    setTime()
+       secondsleft = secondsleft-10;
+       fdbkEl.setAttribute("style", "visibility:visible");
+       fdbkEl.textContent = "wrong!"; 
+       clearInterval(timeInterval)
+       while (i < questionList.length-1) {return startQuiz(i++);}
+        
+    };
+
+    }
+    );
+
+
+
 }
-
-quiz()
-
-});
+);
